7c88730e5a755944d908533450fc4e30f1b6a61c
Nimmala <nimmalanusha@gmail.com>
2026-01-03 19:18:04 +0530

    Update src files with latest changes

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import Kalyan1 from './Anu1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Kalyan/> */}
-    <Kalyan1/>
+    <App /> 
+    {/* <Kalyan/>? */}
+    {/* <Kalyan1/> */}
   </React.StrictMode>
 );
 
